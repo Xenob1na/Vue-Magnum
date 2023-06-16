6aca72878276651266a643ba1d9e265d64670aaa
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2023-06-16 21:10:13 +0700

    init

---
+++ src/components/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="max-w-[1170px] mx-auto flex justify-between">
+        <div class="max-w-[572px] mt-[110px]">
+            <div>
+                <h1 class="font-bold text-[55px] uppercase leading-[130%] text-[#0D2344] mb-[14px]">Грузоперевозки <span class="text-[#F3970A]">по ХМАО и ЯНАО</span></h1>
+                <p class="font-medium text-[20px] leading-[130%] text-[#0D2344]/[0.8] max-w-[528px] mb-[27px]">Мы занимаемся транспортными перевозками по направлению ХМАО и ЯНАО более 18 лет. Знаем наизусть каждый километр дорог. Доставим ваш груз качественно и оперативно!</p>
+            </div>
+            <div class="max-w-[370px] flex justify-between">
+                <button class="text-[16px] text-white text-center bg-[#FFB800] rounded-[12px] py-5 px-[30px] hover:bg-[#FFCA42]">Расчитать стоимость</button>
+                <button class="text-[#FFB800] border-solid border-2 border-[#FFB800] px-[17px] py-4 rounded-[12px] hover:bg-[#FFB800] hover:text-white">Заказать</button>
+            </div>
+        </div>
+        <div class="mt-[30px]">
+            <img src="/img/f1.png" alt="">
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Hero from '../components/Hero.vue';
+</script>
+
+<template>
+  <Hero />
+</template>
